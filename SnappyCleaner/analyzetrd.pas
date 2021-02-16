93b07806c2fddcc82371b03064da18a69b7c02e0
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2021-02-16 16:20:02 +0300

    Add files via upload

---
+++ SnappyCleaner/analyzetrd.pas
@@ -0,0 +1,489 @@
+unit AnalyzeTRD;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, Forms, Controls, SysUtils, Process, StdCtrls;
+
+type
+  StartAnalyze = class(TThread)
+  private
+
+
+    { Private declarations }
+  protected
+  var
+    Result: TStringList;
+
+    procedure Execute; override;
+
+    procedure ShowRootTMP;
+    procedure ShowUserTrash;
+    procedure ShowUserTMP;
+    procedure ShowRecentDocuments;
+    procedure ShowURPMICache;
+    procedure ShowMozillaCache;
+    procedure ShowChromeCache;
+    procedure ShowOperaCache;
+    procedure ShowChromiumCache;
+    procedure ShowPaleMoonCache;
+    procedure ShowHDDUsage;
+
+    procedure ShowNewKernel;
+
+    procedure ShowRemKernels;
+    procedure ShowRemKernelsSize;
+
+    procedure ShowRemOrphans;
+    procedure ShowRemOrphansSize;
+
+    procedure StopAnalyzeProcess;
+
+  end;
+
+implementation
+
+uses Unit1;
+
+{ TRD }
+
+procedure StartAnalyze.Execute;
+var
+  ExProcess: TProcess;
+begin
+  try
+    FreeOnTerminate := True;
+
+    //Результирующий список
+    Result := TStringList.Create;
+
+    ExProcess := TProcess.Create(nil);
+    ExProcess.Executable := '/usr/bin/sh';
+    ExProcess.Options := ExProcess.Options + [poUsePipes, poWaitOnExit];
+
+    { ЛЕВЫЙ БЛОК - КОРЗИНА и ПРОЧЕЕ }
+
+    //Каталог /root/tmp
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/root/tmp" ]; then ' +
+      'roottmp=$(/usr/bin/du -csh /root/tmp/* /root/tmp/.[!.]* | tail -n 1 | awk ' +
+      '''{ ' + 'print $1' + ' }''' + '); else roottmp="no"; fi; echo $roottmp');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер /root/tmp
+    Synchronize(@ShowRootTMP);
+
+    //Корзина пользователя
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add(
+      'if [[ -n $(/usr/bin/find /run/media -type d -name ".Trash-1000") ]]; ' +
+      'then flash="$(/usr/bin/find /run/media -type d -name ".Trash-1000")/files/* ' +
+      '$(/usr/bin/find /run/media -type d -name ".Trash-1000")/files/.[!.]* ' +
+      '$(/usr/bin/find /run/media -type d -name ".Trash-1000")/info/* ' +
+      '$(/usr/bin/find /run/media -type d -name ".Trash-1000")/info/.[!.]*"; else flash=""; fi; '
+      + 'if [[ -n $(/usr/bin/find /run/media -type d -name ".Trash-500") ]]; ' +
+      'then flash="$flash $(/usr/bin/find /run/media -type d -name ".Trash-500")/files/* '
+      + '$(/usr/bin/find /run/media -type d -name ".Trash-500")/files/.[!.]* ' +
+      '$(/usr/bin/find /run/media -type d -name ".Trash-500")/info/* ' +
+      '$(/usr/bin/find /run/media -type d -name ".Trash-500")/info/.[!.]*"; else flash=""; fi; '
+      + 'if [ -d "/home/' + ActUser[0] +
+      '/.local/share/Trash" ]; then if [ -d "/home/' + ActUser[0] +
+      '/.local/share/Trash/files" ]; then ' + 'trash=$(/usr/bin/du -csh /home/' +
+      ActUser[0] + '/.local/share/Trash/files/* /home/' + ActUser[0] +
+      '/.local/share/Trash/files/.[!.]* ' + '/home/' + ActUser[0] +
+      '/.local/share/Trash/info/* /home/' + ActUser[0] +
+      '/.local/share/Trash/info/.[!.]* $flash | tail -n 1 | awk ' +
+      '''{ ' + 'print $1' + ' }''' +
+      '); else trash="0"; fi; else trash="no"; fi; echo $trash');
+
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер корзины
+    Synchronize(@ShowUserTrash);
+
+    //Временные файлы пользователя
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/home/' + ActUser[0] +
+      '/tmp" ]; then ' + 'usertmp=$(/usr/bin/du -csh /home/' + ActUser[0] +
+      '/tmp/* /home/' + ActUser[0] + '/tmp/.[!.]* | tail -n 1 | awk ' +
+      '''{ ' + 'print $1' + ' }''' + '); else usertmp="no"; fi; echo $usertmp');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер временных файлов пользователя
+    Synchronize(@ShowUserTMP);
+
+    //Каталог RecentDocuments
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/home/' + ActUser[0] +
+      '/.local/share/RecentDocuments" ]; then ' + 'userrdoc=$(/usr/bin/du -csh /home/' +
+      ActUser[0] + '/.local/share/RecentDocuments/* /home/' +
+      ActUser[0] + '/.local/share/RecentDocuments/.[!.]* | tail -n 1 | awk ' +
+      '''{ ' + 'print $1' + ' }''' + '); else userrdoc="no"; fi; echo $userrdoc');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер RecentDocuments
+    Synchronize(@ShowRecentDocuments);
+
+    //Каталог кеш-URPMI
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/var/cache/urpmi" ]; then ' +
+      'urpmicache=$(/usr/bin/du -csh /var/cache/urpmi/* /var/cache/urpmi/.[!.]* | tail -n 1 | awk '
+      + '''{ ' + 'print $1' + ' }''' + '); else urpmicache="no"; fi; echo $urpmicache');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер кеша URPMI
+    Synchronize(@ShowURPMICache);
+
+    //Кеш Mozilla Firefox
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/home/' + ActUser[0] +
+      '/.cache/mozilla" ]; then ' + 'mozcache=$(/usr/bin/du -csh /home/' +
+      ActUser[0] + '/.cache/mozilla/* /home/' + ActUser[0] +
+      '/.cache/mozilla/.[!.]* | tail -n 1 | awk ' + '''{ ' + 'print $1' +
+      ' }''' + '); else mozcache="no"; fi; echo $mozcache');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер кеша Mozilla
+    Synchronize(@ShowMozillaCache);
+
+    //Кеш Google Chrome
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/home/' + ActUser[0] +
+      '/.cache/google-chrome" ]; then ' + 'chromecache=$(/usr/bin/du -csh /home/' +
+      ActUser[0] + '/.cache/google-chrome/* /home/' + ActUser[0] +
+      '/.cache/google-chrome/.[!.]* | tail -n 1 | awk ' + '''{ ' +
+      'print $1' + ' }''' + '); else chromecache="no"; fi; echo $chromecache');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер кеша Chrome
+    Synchronize(@ShowChromeCache);
+
+    //Кеш Opera
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/home/' + ActUser[0] +
+      '/.cache/opera" ]; then ' + 'operacache=$(/usr/bin/du -csh /home/' +
+      ActUser[0] + '/.cache/opera/* /home/' + ActUser[0] +
+      '/.cache/opera/.[!.]* | tail -n 1 | awk ' + '''{ ' + 'print $1' +
+      ' }''' + '); else operacache="no"; fi; echo $operacache');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер кеша Opera
+    Synchronize(@ShowOperaCache);
+
+    //Кеш Chromium
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/home/' + ActUser[0] +
+      '/.cache/chromium" ]; then ' + 'chromiumcache=$(/usr/bin/du -csh /home/' +
+      ActUser[0] + '/.cache/chromium/* /home/' + ActUser[0] +
+      '/.cache/chromium/.[!.]* | tail -n 1 | awk ' + '''{ ' +
+      'print $1' + ' }''' + '); else chromiumcache="no"; fi; echo $chromiumcache');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер кеша Chromium
+    Synchronize(@ShowChromiumCache);
+
+    //Кеш PaleMoon
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('if [ -d "/home/' + ActUser[0] +
+      '/.cache/moonchild productions" ]; then ' +
+      'palemooncache=$(/usr/bin/du -csh "/home/' + ActUser[0] +
+      '/.cache/moonchild productions/"* "/home/' + ActUser[0] +
+      '/.cache/moonchild productions/.[!.]"* | tail -n 1 | awk ' +
+      '''{ ' + 'print $1' + ' }''' +
+      '); else palemooncache="no"; fi; echo $palemooncache');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем размер кеша Palemoon
+    Synchronize(@ShowPaleMoonCache);
+
+
+    //Использование HDD
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('/usr/bin/df / --output=pcent | tail -n 1 | cut -d% -f1');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем использование HDD
+    Synchronize(@ShowHDDUsage);
+
+
+    { ПРАВЫЙ БЛОК - ЯДРА И СИРОТЫ }
+
+    //Узнаём тип ядра
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('for ktype in "server" "desktop" "linus"; do ' +
+      'if [ -n "$(echo $(uname -r) | grep $ktype)" ]; then break; fi; done; echo $ktype');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output); //Result[0] - тип ядра
+
+    //Начитываем список всех ядер
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('allkernel=$(rpm -qa kernel-' + Result[0] +
+      '* --qf ' + '''%{name} %{installtime}\n''' + ' | ' +
+      'grep -v "latest" | sort -rnk 2 | awk ' + '''{ print $1 }''' +
+      '); echo "$allkernel"');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Выгружаем в файл, готовимся оставить только ядра на удаление
+    Result.SaveToFile(ExtractFilePath(ParamStr(0)) + 'tmp/remkernel');
+
+    //Выделяем новое ядро
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('actkernel=$(echo "' + Trim(Result.Text) +
+      '" | grep -v "devel" | head -n 1); echo "$actkernel"');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+    //Показываем новое ядро
+    Synchronize(@ShowNewKernel);
+
+    //Вычислем сигнатуру нового ядра
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('signature=$(echo ' + Result[0] +
+      '| sed ' + '''s/[^0-9]/' + '/g''' + '); echo $signature');
+    ExProcess.Execute;
+    Result.LoadFromStream(ExProcess.Output);
+
+    //Удаляем из списка имена ядер, если сигнатура >= сигнатуре нового ядра (-ge)
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('while read LINE; do ' + 'if [[ $(echo "$LINE" | sed ' +
+      '''s/[^0-9]/' + '/g''' + ') -ge "' + Result[0] + '" ]]; then ' +
+      'sed -i "/$LINE/d" "' + ExtractFilePath(ParamStr(0)) +
+      'tmp/remkernel"; ' + 'fi; done < "' + ExtractFilePath(ParamStr(0)) +
+      'tmp/remkernel"');
+    ExProcess.Execute;
+    //Показываем список удаляемых ядер
+    Synchronize(@ShowRemKernels);
+
+    //Рассчитываем размер удаляемых ядер
+    if Trim(MainForm.RemKernelBox.Items.Text) <> '' then
+    begin
+      ExProcess.Parameters.Clear;
+      ExProcess.Parameters.Add('-c');
+      ExProcess.Parameters.Add('klsize=$(/usr/bin/rpm -qi ' +
+        StringReplace(MainForm.RemKernelBox.Items.Text, #10, ' ', [rfReplaceAll]) +
+        ' | grep "Size" | cut -f 2 -d : -s | cut -c 2-); ' +
+        'klsize=$(echo $klsize | sed ' + '''s/ /+/g''' +
+        '); if [[ "$klsize" -lt "1048576" ]]; then let "klsize=($klsize) / 1024"; ' +
+        'echo ${klsize}K; else let "klsize=($klsize) / 1048576"; echo ${klsize}M; fi;');
+      ExProcess.Execute;
+      Result.LoadFromStream(ExProcess.Output);
+    end
+    else
+      Result.Insert(0, '0');
+    //Показываем размер удаляемых ядер
+    Synchronize(@ShowRemKernelsSize);
+
+    //Сохраняем список urpmq --auto-orphans в ../auto-orphans
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('/usr/bin/urpmq --auto-orphans > ' +
+      ExtractFilePath(ParamStr(0)) + 'tmp/auto-orphans');
+    ExProcess.Execute;
+
+    //Исключаем из списка auto-orphans старые ядра
+    ExProcess.Parameters.Clear;
+    ExProcess.Parameters.Add('-c');
+    ExProcess.Parameters.Add('/usr/bin/grep -f ' + ExtractFilePath(ParamStr(0)) +
+      'tmp/remkernel ' + '-vFx ' + ExtractFilePath(ParamStr(0)) +
+      'tmp/auto-orphans > ' + ExtractFilePath(ParamStr(0)) +
+      'tmp/auto-orphans-tmp; ' + 'cp -f ' + ExtractFilePath(ParamStr(0)) +
+      'tmp/auto-orphans-tmp ' + ExtractFilePath(ParamStr(0)) + 'tmp/auto-orphans');
+    ExProcess.Execute;
+    //Показываем список сирот
+    Synchronize(@ShowRemOrphans);
+
+    //Рассчитываем вес осиротевших пакетов
+    if MainForm.AutoOrphansBox.Count > 0 then
+    begin
+      ExProcess.Parameters.Clear;
+      ExProcess.Parameters.Add('-c');
+      ExProcess.Parameters.Add('orpsize=$(/usr/bin/rpm -qi ' +
+        StringReplace(MainForm.AutoOrphansBox.Items.Text, #10, ' ', [rfReplaceAll]) +
+        ' | grep "Size" | cut -f 2 -d : -s | cut -c 2-); ' +
+        'orpsize=$(echo $orpsize | sed ' + '''s/ /+/g''' +
+        '); if [[ "$orpsize" -lt "1048576" ]]; then let "orpsize=($orpsize) / 1024"; ' +
+        'echo ${orpsize}K; else let "orpsize=($orpsize) / 1048576"; echo ${orpsize}M; fi;');
+      ExProcess.Execute;
+      Result.LoadFromStream(ExProcess.Output);
+    end
+    else
+      Result.Insert(0, '0');
+    //Показываем размер осиротевших пакетов
+    Synchronize(@ShowRemOrphansSize);
+
+  finally
+    Synchronize(@StopAnalyzeProcess);
+    ExProcess.Free;
+    Result.Free;
+    Terminate;
+  end;
+end;
+
+
+{ БЛОК ОТОБРАЖЕНИЯ РЕЗУЛЬТАТОВ }
+
+procedure StartAnalyze.ShowRootTMP;
+begin
+  //Каталог /root/tmp
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label24.Caption := 'нет'
+  else
+    MainForm.Label24.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowUserTrash;
+begin
+  //Показываем размер корзины
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label10.Caption := 'нет'
+  else
+    MainForm.Label10.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowUserTMP;
+begin
+  //Показываем вес временных файлов:
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label12.Caption := 'нет'
+  else
+    MainForm.Label12.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowRecentDocuments;
+begin
+  //Показываем размер RecentDocuments:
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label14.Caption := 'нет'
+  else
+    MainForm.Label14.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowURPMICache;
+begin
+  //Показываем размер кеша URPMI
+  MainForm.Label16.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowMozillaCache;
+begin
+  //Показываем размер кеша Mozilla
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label18.Caption := 'нет'
+  else
+    MainForm.Label18.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowChromeCache;
+begin
+  //Показываем размер кеша Chrome
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label20.Caption := 'нет'
+  else
+    MainForm.Label20.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowOperaCache;
+begin
+  //Показываем размер кеша Opera
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label22.Caption := 'нет'
+  else
+    MainForm.Label22.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowChromiumCache;
+begin
+  //Показываем размер кеша Chromium
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label23.Caption := 'нет'
+  else
+    MainForm.Label23.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowPaleMoonCache;
+begin
+  //Показываем размер кеша Chromium
+  if (Result[0] = 'no') and (MainForm.LangBtn.Caption = 'EN') then
+    MainForm.Label27.Caption := 'нет'
+  else
+    MainForm.Label27.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowHDDUsage;
+begin
+  //Показываем использование диска
+  MainForm.Label26.Caption := Trim(Result[0]) + '%';
+end;
+
+procedure StartAnalyze.ShowNewKernel;
+begin
+  //Показываем активное ядро
+  MainForm.Label2.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowRemKernels;
+begin
+  //Показываем список удаляемых ядер
+  MainForm.RemKernelBox.Items.LoadFromFile(ExtractFilePath(ParamStr(0)) +
+    'tmp/remkernel');
+end;
+
+procedure StartAnalyze.ShowRemKernelsSize;
+begin
+  //Показываем вес удаляемых ядер
+  MainForm.Label6.Caption := Result[0];
+end;
+
+procedure StartAnalyze.ShowRemOrphans;
+begin
+  //Показываем список осиротевших пакетов
+  MainForm.AutoOrphansBox.Items.LoadFromFile(ExtractFilePath(ParamStr(0)) +
+    'tmp/auto-orphans');
+end;
+
+procedure StartAnalyze.ShowRemOrphansSize;
+begin
+  //Показываем вес пакетов-сирот
+  MainForm.Label1.Caption := Result[0];
+end;
+
+procedure StartAnalyze.StopAnalyzeProcess;
+begin
+  //Скрываем прогресс
+  with MainForm do
+  begin
+    Timer1.Enabled := False;
+    ProgressBar1.Visible := False;
+    ProgressBar1.Position := 0;
+
+    Panel1.Enabled := True;
+    Panel2.Enabled := True;
+    CleanBtn.Enabled := True;
+
+    if LangBtn.Caption = 'EN' then
+      StaticText1.Caption := 'анализ завершен...'
+    else
+      StaticText1.Caption := 'analysis completed...';
+
+    AnalyzeBtn.SetFocus;
+  end;
+end;
+
+end.
---
+++ SnappyCleaner/depstrd.pas
@@ -0,0 +1,170 @@
+unit DepsTRD;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Process;
+
+type
+  StartShowDeps = class(TThread)
+  private
+
+    { Private declarations }
+  protected
+  var
+    ListTMP: TStringList;
+    //Прототип списка с исключенными дубликатами и сортировкой
+
+    procedure Execute; override;
+    procedure ShowLeftList;
+    procedure ShowRightList;
+    procedure ShowRemoveStart;
+    procedure ShowRemoveFinal;
+  end;
+
+implementation
+
+uses Unit1, DepsUnit;
+
+{ TRD }
+
+procedure StartShowDeps.Execute;
+var
+  ExProcess: TProcess;
+begin
+  try
+    //Показываем старт процесса
+    Synchronize(@ShowRemoveStart);
+
+    //Временный правый список для исключения дубликатов
+    ListTMP := TStringList.Create;
+    ListTMP.Sorted := True; //Сортируем
+    ListTMP.Duplicates := dupIgnore; //Запрещаем дубликаты
+
+    FreeOnTerminate := True;
+
+    ExProcess := TProcess.Create(nil);
+    ExProcess.Executable := '/usr/bin/sh';
+    ExProcess.Options := ExProcess.Options + [poUsePipes, poWaitOnExit];
+    ExProcess.Parameters.Add('-c');
+
+    //Закидываем файл-флаг начала процесса
+    ExProcess.Parameters.Add('echo "$$" > ' + ExtractFilePath(ParamStr(0)) +
+      'tmp/process');
+
+    ExProcess.Execute;
+
+    //Левый список (требуются для этого пакета)
+    ExProcess.Parameters.Delete(1);
+    ExProcess.Parameters.Add('/usr/bin/urpmq --requires-recursive ' +
+      Package + ' | grep -v "' + Package + '"');
+    ExProcess.Execute;
+    ListTMP.LoadFromStream(ExProcess.Output);
+    //Показываем левый список
+    Synchronize(@ShowLeftList);
+
+    //Правый список формируется из установленных (этот пакет требуется для...)
+    ExProcess.Parameters.Delete(1);
+
+    ExProcess.Parameters.Add('for i in $(/usr/bin/urpmq --whatrequires ' +
+      Package + '); do /usr/bin/rpm -q $i --qf "%{name}\n"; ' +
+      'if [ ! -f "' + ExtractFilePath(ParamStr(0)) +
+      'tmp/process" ]; then break; fi; done ' + '| grep -v "' + Package + '"');
+
+    ExProcess.Execute;
+    ListTMP.LoadFromStream(ExProcess.Output);
+    //Показываем правый список
+    Synchronize(@ShowRightList);
+
+  finally
+    Synchronize(@ShowRemoveFinal);
+    ListTMP.Free;
+    ExProcess.Free;
+    Terminate;
+  end;
+end;
+
+
+{ БЛОК ОТОБРАЖЕНИЯ ПРОЦЕССА }
+
+procedure StartShowDeps.ShowRemoveStart;
+begin
+  //Показываем старт процесса
+  DepsForm.SEdit.Enabled := False;
+  DepsForm.SEdit.Button.Enabled := False;
+
+  with MainForm do
+  begin
+    Panel1.Enabled := False;
+    Panel2.Enabled := False;
+
+    ProgressBar1.Visible := True;
+    Timer1.Enabled := True;
+
+    if LangBtn.Caption = 'EN' then
+    begin
+      DepsForm.Caption := 'Пакет: ' + Package;
+      StaticText1.Caption :=
+        'запущен анализ зависимостей пакета, ждите...';
+    end
+    else
+    begin
+      DepsForm.Caption := 'Package: ' + Package;
+      StaticText1.Caption := 'run analysis of the package dependencies, wait...';
+    end;
+  end;
+end;
+
+procedure StartShowDeps.ShowLeftList;
+begin
+  //Показываем левый список
+  DepsForm.ListBox1.Items.Assign(ListTMP);
+end;
+
+procedure StartShowDeps.ShowRightList;
+begin
+  //Показываем правый список
+  DepsForm.ListBox2.Items.Assign(ListTMP);
+end;
+
+//Финал очистки
+procedure StartShowDeps.ShowRemoveFinal;
+begin
+  //Скрываем прогресс
+  with MainForm do
+  begin
+    Timer1.Enabled := False;
+    ProgressBar1.Visible := False;
+    ProgressBar1.Position := 0;
+
+    Panel1.Enabled := True;
+    Panel2.Enabled := True;
+
+    DepsForm.SEdit.Enabled := True;
+    DepsForm.SEdit.Button.Enabled := True;
+
+    if LangBtn.Caption = 'EN' then
+      StaticText1.Caption :=
+        'анализ зависимостей пакета завершен...'
+    else
+      StaticText1.Caption := 'dependency analysis package is completed...';
+  end;
+
+  if DepsForm.ListBox1.Items.Count > 0 then
+    DepsForm.ListBox1.ItemIndex := 0;
+
+  if DepsForm.ListBox2.Items.Count > 0 then
+    DepsForm.ListBox2.ItemIndex := 0;
+
+  if FileExists(ExtractFilePath(ParamStr(0)) + 'tmp/process') then
+  begin
+    DepsForm.Show;
+
+    //Снимаем флаг, процесс завершен
+    DeleteFile(ExtractFilePath(ParamStr(0)) + 'tmp/process');
+  end;
+end;
+
+end.
